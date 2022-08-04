b2d3aba9f30ba4d42483b1f3f30893542e2e6d71
Adriano Paduam de Souza <adriano.paduam@outlook.com>
2022-08-04 02:45:00 -0300

    fix: column names

--- dags/domain/data/razac/tables/shipdate/shipdate_types.py
+++ dags/domain/data/razac/tables/shipdate/shipdate_types.py
@@ -1,9 +1,9 @@
 razac_shipdate_types = {
-    "ID": "Int64",
-    "Cod Transmissão DI": "Int64",
-    "Qtde FCL 20": "Int64",
-    "Qtde FCL 40": "Int64",
-    "Free Time": "Int64",
-    "PACKS": "Int64",
-    "TT - Transit Time": "Int64",
+    "impo_id_processo": "Int64",
+    "impo_cd_transmissao_di": "Int64",
+    "impo_qt_fcl_20": "Int64",
+    "impo_qt_fcl_40": "Int64",
+    "impo_nm_free_time": "Int64",
+    "impo_qt_packs": "Int64",
+    "impo_qt_transit_time": "Int64",
 }
