f61c22a8bb5c0e85dee2b06a96ac90d760f813c3
Adriano Paduam de Souza <adriano.paduam@outlook.com>
2022-08-04 01:53:13 -0300

    fix: missing connection

--- dags/infrastructure/connections/postgres_connector.py
+++ dags/infrastructure/connections/postgres_connector.py
@@ -180,7 +180,7 @@ class PostgresConnector(SQLConnector):
                 DataFrame(
                     [{key: value for key, value in info_row.items() if key not in self.internal_control_columns}],
                     index=index_column,
-                ).to_sql(target_table, if_exists="append")
+                ).to_sql(target_table, session.connection(), if_exists="append")
                 # nan_replacement = "NULL"
                 # info_row = {key: nan_replacement if str(value) == "nan" else value for key, value in info_row.items()}
                 # info_row = {
