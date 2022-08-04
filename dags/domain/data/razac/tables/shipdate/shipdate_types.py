b9b775292ba233392ae48ee64852e2cd02e41eb6
Adriano Paduam de Souza <adriano.paduam@outlook.com>
2022-08-04 02:57:32 -0300

    fix: removing wrong column

--- dags/domain/data/razac/tables/shipdate/shipdate_types.py
+++ dags/domain/data/razac/tables/shipdate/shipdate_types.py
@@ -2,7 +2,6 @@ razac_shipdate_types = {
     "impo_cd_transmissao_di": "Int64",
     "impo_qt_fcl_20": "Int64",
     "impo_qt_fcl_40": "Int64",
-    "impo_nm_free_time": "Int64",
     "impo_qt_packs": "Int64",
     "impo_qt_transit_time": "Int64",
 }
